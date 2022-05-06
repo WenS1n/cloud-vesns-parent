486758ba3208751ff0925a1089b1474cd185038a
WenS1n <865047755@qq.com>
2022-05-06 15:13:18 +0800

    update

--- cloud-service/cloud-service-edu/src/test/java/cn/vesns/eduservice/excel/ExcelTest.java
+++ cloud-service/cloud-service-edu/src/test/java/cn/vesns/eduservice/excel/ExcelTest.java
@@ -11,7 +11,7 @@ public class ExcelTest {
     @Test
     public void writeTest(){
         // 写法1
-        String fileName = "F:\\11.xlsx";
+        String fileName = "F:\\113.xlsx";
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         // 如果这里想使用03 则 传入excelType参数即可
         EasyExcel.write(fileName, DemoData.class).sheet("学生列表").doWrite(data());
